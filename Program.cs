f34b29e84505f544a69b14ce73dab6e0c08953ff
JakobGP <123357354+JakobGP@users.noreply.github.com>
2023-05-08 08:51:23 +0200

    Add project files.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+namespace Programmerings_eksamensprojekt
+{
+    internal class Programmerings_eksamensprojekt
+    {
+        static void Main(string[] args)
+        {
+            string type_træning = "";
 
-//Yeet
+            Console.WriteLine("Velkommen til fysik træneren");
+            Console.WriteLine("Hvilken type opgaver vil du lave? Træning eller MC (multiple choice)");
+            type_træning = Console.ReadLine();
+
+            if (type_træning.ToLower() == "mc")
+            {
+                MC();
+            }
+        }
+
+        static void MC()
+        {
+            Console.WriteLine("Multiple Choice");
+            Random tal = new Random();
+            int spørgsmål = tal.Next(1, 7);
+            switch (spørgsmål)
+            {
+                case 1:
+                    //førse spørgsmål
+                    break;
+
+                case 2:
+                    // andet spørgsmål
+                    break;
+
+                case 3:
+                    break;
+
+
+                    //Yeet
+            }
+        }
+
+    }
+}
